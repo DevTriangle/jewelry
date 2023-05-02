d4b5a3aee7da2c54c721b1689bee2a5d3e26952c
DevTriangle <halbert370@gmail.com>
2023-05-02 09:28:27 +0300

    ß

---
+++ lib/view/widgets/app_text_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:jewelry/view/colors.dart';
+
+class AppTextField extends StatelessWidget {
+  final int maxLines;
+  final int minLines;
+  final int maxLength;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final String hintText;
+  final TextStyle hintStyle;
+
+  const AppTextField({
+    super.key,
+    required this.maxLines,
+    required this.minLines,
+    required this.maxLength,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.hintText = "",
+    this.hintStyle = TextStyle(
+      color: AppColors.hintColor
+    )
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      maxLines: maxLines,
+      minLines: minLines,
+      maxLength: maxLength,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: AppColors.cardColor,
+        counterText: "",
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+        hintText: hintText,
+        hintStyle: hintStyle,
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
+            width: 0
+          ),
+          borderRadius: BorderRadius.circular(16.0)
+        )
+      ),
+    );
+  }
+
+}
