f0d4fcd32f06e1982a23caa9745c972d46086b5b
DevTriangle <halbert370@gmail.com>
2023-05-02 14:52:40 +0300

    ß

--- lib/view/colors.dart
+++ lib/view/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primary = Color(0xFF2EB6FF);
+  static const Color primary = Color(0xFFFFA040);
   static const Color background = Color(0xFFFFFFFF);
   static const Color cardColor = Color(0xFFF3F3F3);
   static const Color hintColor = Color(0xFF676767);
