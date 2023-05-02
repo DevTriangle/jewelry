0d23b5ad5a149090cc392e04a21f3ff1c404d2ef
Triangle <halbert370@gmail.com>
2023-05-02 09:49:19 +0300

    .

--- lib/view/screens/auth_screen.dart
+++ lib/view/screens/auth_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jewelry/view/colors.dart';
-import 'package:jewelry/view/widgets/AppButton.dart';
+import 'package:jewelry/view/widgets/app_button.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AuthScreen extends StatefulWidget {
