848bd05fdc885a775b2e51270b7e34e99fffa477
Triangle <halbert370@gmail.com>
2023-05-02 08:52:16 +0300

    Initial

---
+++ android/app/src/main/kotlin/com/triangle/jewelry/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.triangle.jewelry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/screens/auth_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => AuthScreenState();
+}
+
+class AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
